210695396e84267787b58be8e7284ff3e655ee2b
bielak7 <92972176+bielak7@users.noreply.github.com>
2022-05-02 12:34:17 +0200

    logowanie do płatnika (proof of concept)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from pywinauto.application import Application
+from pywinauto import Application
 import time
 
 
@@ -9,10 +9,19 @@ if __name__ == '__main__':
     window = app.window(title="Aktualizacja Programu Płatnik").wait("visible", timeout=20, retry_interval=1)
 
     time.sleep(10)
-    error_dlg = window.children()[0]
+    error_dlg = Window.children()[0]
     error_dlg.children()[0].click()
     time.sleep(5)
 
     app2 = Application(backend="uia").connect(title="Logowanie do programu", class_name="05381030-963D-4779-BECA-0D7D49268EDB-Login")
     app2.windows()[0].children()[0].set_text("Abcd1234%")
     app2.windows()[0].children()[4].click()
+    app2 = Application(backend="uia").connect(title="Płatnik - Moon")
+    # print(app2.windows())
+    # print(app2.windows()[0].children())
+    app2.windows()[0].children()[4].click()
+    # print(app2.windows())
+    # print(app2.windows()[0].children())
+    # print(app2.windows()[0].children()[0].children())
+    # print(app2.windows()[0].children()[0].children()[0].expand())
+
