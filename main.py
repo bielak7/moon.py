315b384159a3cb9cfc41e8545e9276f22b848606
bielak7 <92972176+bielak7@users.noreply.github.com>
2022-05-02 10:37:15 +0200

    Changes by User

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-import subprocess
+from pywinauto.application import Application
+import time
 
-subprocess.Popen("C:\\Program Files (x86)\\Asseco Poland SA\\Platnik\\P2Start.exe")
+
+if __name__ == '__main__':
+
+    app = Application(backend="uia").start(
+        "C:\Program Files (x86)\Asseco Poland SA\Platnik\P2Start.exe")  # Kod szukjacy execa
+    window = app.window(title="Aktualizacja Programu Płatnik").wait("visible", timeout=20, retry_interval=1)
+
+    time.sleep(10)
+    error_dlg = window.children()[0]
+    error_dlg.children()[0].click()
+    time.sleep(5)
+
+    app2 = Application(backend="uia").connect(title="Logowanie do programu", class_name="05381030-963D-4779-BECA-0D7D49268EDB-Login")
+    app2.windows()[0].children()[0].set_text("Abcd1234%")
+    app2.windows()[0].children()[4].click()
