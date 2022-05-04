d79090e66435e2f9dfbad5b6721c2ec23a642840
bielak7 <92972176+bielak7@users.noreply.github.com>
2022-05-04 12:18:03 +0200

    logowanie do płatnika (proof of concept) + anulowanie aktualizacji

--- main.py
+++ main.py
@@ -4,11 +4,10 @@ import time
 
 if __name__ == '__main__':
 
-    app = Application(backend="uia").start(
-        "C:\Program Files (x86)\Asseco Poland SA\Platnik\P2Start.exe")  # Kod szukjacy execa
-    window = app.window(title="Aktualizacja Programu Płatnik").wait("visible", timeout=20, retry_interval=1)
+    app = Application(backend="uia").start("C:\Program Files (x86)\Asseco Poland SA\Platnik\P2Start.exe")  # Kod szukjacy execa
+    Window = app.window(title="Aktualizacja Programu Płatnik").wait("visible", timeout=10, retry_interval=1) # timeout was 20
 
-    time.sleep(10)
+    time.sleep(10) # was 10
     error_dlg = Window.children()[0]
     error_dlg.children()[0].click()
     time.sleep(5)
@@ -16,12 +15,18 @@ if __name__ == '__main__':
     app2 = Application(backend="uia").connect(title="Logowanie do programu", class_name="05381030-963D-4779-BECA-0D7D49268EDB-Login")
     app2.windows()[0].children()[0].set_text("Abcd1234%")
     app2.windows()[0].children()[4].click()
-    app2 = Application(backend="uia").connect(title="Płatnik - Moon")
+
+    ac_window = app2.window(title="Aktualizacja programu i danych płatnika").wait("visible", timeout=5, retry_interval=1)
+    ac_window.children()[4].click()
+    # error_dlg = Window.children()[4].click()
+    # app2 = Application(backend="uia").connect(title="Płatnik - Moon")
+    # app4 = Application(backend='uia').connect(class_name='TCorelButton')
     # print(app2.windows())
     # print(app2.windows()[0].children())
-    app2.windows()[0].children()[4].click()
+
+    # app2.windows()[0].children()[4].click()
     # print(app2.windows())
     # print(app2.windows()[0].children())
     # print(app2.windows()[0].children()[0].children())
     # print(app2.windows()[0].children()[0].children()[0].expand())
-
+    app3 = Application(backend='uia').connect(title="Menu główne")
